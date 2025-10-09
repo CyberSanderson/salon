19b5f44753d27fed6880db3fc73cd2559b4b0d06
CyberSanderson <cyberopsanderson@gmail.com>
2025-10-09 08:21:02 -0400

    added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Script from 'next/script'
 import './globals.css'
-import Footer from '@/components/footer' // <-- The fix is here (Capital 'F')
+import Footer from '@/components/footer' 
 import Header from '@/components/Header'
 
 const poppins = Poppins({
@@ -23,6 +23,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1441828204616933&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Header />
         <main>{children}</main>
         <Footer />
@@ -38,6 +48,22 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-J6VV76BZ4W');
+          `}
+        </Script>
+
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1441828204616933');
+            fbq('track', 'PageView');
           `}
         </Script>
       </body>
